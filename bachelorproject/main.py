939328686a736be5151124ee13f86b93fcf33d2c
Matthijs <matthijs.vanderlende@gmail.com>
2024-03-22 11:55:00 +0100

    small addition

--- bachelorproject/main.py
+++ bachelorproject/main.py
@@ -1,10 +1,5 @@
-from stable_baselines3.dqn import MlpPolicy
-
-from agentfactory.agentfactory import AgentFactory
-import gymnasium as gym
 
 from loops.envinteraction import env_interaction_gym
-from metricstracker.metricstracker import MetricsTracker
 
 if __name__ == "__main__":
     env_interaction_gym("gaussian_dp_agent", "MountainCar-v0", 3000)
