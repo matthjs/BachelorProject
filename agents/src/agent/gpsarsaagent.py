ecf715c34094dd96d516e4ed3b7c5c5901085e6f
Matthijs <matthijs.vanderlende@gmail.com>
2024-03-22 11:56:40 +0100

    added GP-SARSA template

---
+++ agents/src/agent/gpsarsaagent.py
@@ -0,0 +1,13 @@
+from agent.abstractagent import AbstractAgent
+from agent.valueagent import ValueAgent
+
+
+class GPSarsaAgent(AbstractAgent):
+    def add_trajectory(self, trajectory: tuple) -> None:
+        pass
+
+    def update(self):
+        pass
+
+    def policy(self, state):
+        pass
