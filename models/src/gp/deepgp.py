fa2eb39ca7be2974f1314aade5184742a9102896
Matthijs <matthijs.vanderlende@gmail.com>
2024-05-09 21:33:36 +0200

    deep gp class

---
+++ models/src/gp/deepgp.py
@@ -0,0 +1,128 @@
+"""
+Based on https://github.com/pytorch/botorch/issues/1750 and
+https://docs.gpytorch.ai/en/stable/examples/05_Deep_Gaussian_Processes/Deep_Gaussian_Processes.html
+"""
+from math import floor
+
+import gpytorch
+import torch
+from botorch.acquisition import UpperConfidenceBound
+
+from gpytorch.distributions import MultivariateNormal
+from gpytorch.kernels import RBFKernel, ScaleKernel
+from gpytorch.likelihoods import GaussianLikelihood
+from gpytorch.means import ConstantMean, LinearMean
+
+from gpytorch.models.deep_gps import DeepGP, DeepGPLayer
+from gpytorch.variational import CholeskyVariationalDistribution, VariationalStrategy
+
+from botorch.models.gpytorch import GPyTorchModel
+
+
+class DeepGPHiddenLayer(DeepGPLayer):
+    """
+    This is essentially a GP using variational inference.
+    """
+
+    def __init__(self, input_dims, output_dims, num_inducing=128, mean_type="constant"):
+        if output_dims is None:
+            inducing_points = torch.randn(num_inducing, input_dims)
+            batch_shape = torch.Size([])
+        else:
+            inducing_points = torch.randn(output_dims, num_inducing, input_dims)
+            batch_shape = torch.Size([output_dims])
+
+        variational_distribution = CholeskyVariationalDistribution(
+            num_inducing_points=num_inducing, batch_shape=batch_shape
+        )
+
+        variational_strategy = VariationalStrategy(
+            self,
+            inducing_points,
+            variational_distribution,
+            learn_inducing_locations=True,
+        )
+
+        super(DeepGPHiddenLayer, self).__init__(
+            variational_strategy, input_dims, output_dims
+        )
+
+        if mean_type == "constant":
+            self.mean_module = ConstantMean(batch_shape=batch_shape)
+        else:
+            self.mean_module = LinearMean(input_dims)
+        self.covar_module = ScaleKernel(
+            RBFKernel(batch_shape=batch_shape, ard_num_dims=input_dims),
+            batch_shape=batch_shape,
+            ard_num_dims=None,
+        )
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return MultivariateNormal(mean_x, covar_x)
+
+    def __call__(self, x, *other_inputs, **kwargs):
+        """
+        Overriding __call__ isn't strictly necessary, but it lets us add concatenation based skip connections
+        easily. For example, hidden_layer2(hidden_layer1_outputs, inputs) will pass the concatenation of the first
+        hidden layer's outputs and the input data to hidden_layer2.
+        """
+        if len(other_inputs):
+            if isinstance(x, gpytorch.distributions.MultitaskMultivariateNormal):
+                x = x.rsample()
+
+            processed_inputs = [
+                inp.unsqueeze(0).expand(
+                    gpytorch.settings.num_likelihood_samples.value(), *inp.shape
+                )
+                for inp in other_inputs
+            ]
+
+            x = torch.cat([x] + processed_inputs, dim=-1)
+
+        return super().__call__(x, are_samples=bool(len(other_inputs)))
+
+
+class BotorchDeepGP(DeepGP, GPyTorchModel):
+    def __init__(self, train_x_shape, num_hidden_dims=10):
+        hidden_layer = DeepGPHiddenLayer(
+            input_dims=train_x_shape[-1],
+            output_dims=num_hidden_dims,
+            mean_type="linear",
+        )
+
+        last_layer = DeepGPHiddenLayer(
+            input_dims=hidden_layer.output_dims,
+            output_dims=None,
+            mean_type="constant",
+        )
+
+        super().__init__()
+
+        self.hidden_layer = hidden_layer
+        self.last_layer = last_layer
+        self.likelihood = GaussianLikelihood()
+        self._num_outputs = 1
+
+    def forward(self, inputs):
+        hidden_rep1 = self.hidden_layer(inputs)
+        output = self.last_layer(hidden_rep1)
+        return output
+
+    def predict(self, test_loader):
+        with torch.no_grad():
+            mus = []
+            variances = []
+            lls = []
+            for x_batch, y_batch in test_loader:
+                preds = self.likelihood(self(x_batch))
+                mus.append(preds.mean)
+                variances.append(preds.variance)
+                lls.append(self.likelihood.log_marginal(y_batch, self(x_batch)))
+
+        return (
+            torch.cat(mus, dim=-1),
+            torch.cat(variances, dim=-1),
+            torch.cat(lls, dim=-1),
+        )
