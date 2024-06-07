a86e366808e6e88bded8a608c7ea52392a6f08fa
Matthijs <matthijs.vanderlende@gmail.com>
2024-06-07 14:13:20 +0200

    stuff

--- bachelorproject/main.py
+++ bachelorproject/main.py
@@ -18,10 +18,10 @@ def test_load_experiment():
 
 
 if __name__ == "__main__":
-    sim = SimulatorRL("CartPole-v1", experiment_id="experiment_dummy_90")
+    sim = SimulatorRL("CartPole-v1", experiment_id="experiment_dummy_91")
     (sim
      # .register_agent("linear_q_agent_1", "linear_q_agent")
-     .register_agent("gpq_agent_1", "gpq_agent")
+     .register_agent("gpq_agent_3", "gpq_agent")
      # .register_agent("gpsarsa_agent_1", "gpsarsa_agent")
      # .register_agent("gpsarsa_agent_2", "gpsarsa_agent")
      # .register_agent("sb_dqn_1", "sb_dqn")
